bf5c02878649e95615bf3fde2b59b7048deee7b4
anyJohn <33611794+anyJohn@users.noreply.github.com>
2026-04-16 20:50:13 +0800

    refactor: :art: 调整为多项目配置

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-class Config {
-  apiKey: string = process.env.OPENAI_API_KEY || "";
-  baseURL: string = process.env.OPENAI_BASE_URL || "";
-  model: string = process.env.OPENAI_MODEL || "";
-  constructor() {}
-}
